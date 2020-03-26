ee9589812355d587f85be5ca8389b8c087e9c97f
Life4honor <choejy0329@gmail.com>
2020-03-27 00:03:23 +0900

    Update vagrantfile

---
+++ install_kubectl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -sLO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
